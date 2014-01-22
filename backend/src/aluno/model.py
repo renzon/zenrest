387939d9bbba775451d66c6be835a72e8586a132
renzon <renzo@gmail.com>
2014-01-22 15:09:19 -0200

    Salvando aluno

---
+++ backend/src/aluno/model.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+from google.appengine.ext import ndb
+
+
+class Aluno(ndb.Model):
+    nome=ndb.StringProperty(required=True)
+
--- backend/src/web/aluno.py
+++ backend/src/web/aluno.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
+from aluno.model import Aluno
 from zen import router
 
 
@@ -9,5 +10,14 @@ def index(_write_tmpl):
     _write_tmpl('templates/aluno_home.html', values)
 
 
-def form():
-    pass
+def form(_write_tmpl):
+    url = router.to_path(salvar)
+    values = {'aluno_salvar_url': url}
+    _write_tmpl('templates/aluno_form.html', values)
+
+
+def salvar(_handler, nome):
+    aluno=Aluno(nome=nome)
+    aluno.put()
+    home=router.to_path(index)
+    _handler.redirect(home)
