b30eedc936307e45ec5d42776f128d5f11112401
renzon <renzo@gmail.com>
2014-01-22 15:27:48 -0200

    Salvando cursos

---
+++ backend/src/web/curso.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+from curso.model import Curso
+from zen import router
+
+
+def index(_write_tmpl):
+    url = router.to_path(form)
+    values = {'curso_form_url': url}
+    _write_tmpl('templates/curso_home.html', values)
+
+
+def form(_write_tmpl):
+    url = router.to_path(salvar)
+    values = {'curso_salvar_url': url}
+    _write_tmpl('templates/curso_form.html', values)
+
+
+def salvar(_handler, nome):
+    curso=Curso(nome=nome)
+    curso.put()
+    home=router.to_path(index)
+    _handler.redirect(home)
--- backend/src/web/home.py
+++ backend/src/web/home.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
-from web import aluno
+from web import aluno, curso
 from zen import router
 
 
 def index(_write_tmpl):
     url = router.to_path(aluno.index)
-    values = {'aluno_home_url': url}
+    values = {'aluno_home_url': url,
+              'curso_home_url':router.to_path(curso)}
     _write_tmpl('templates/home.html', values)
